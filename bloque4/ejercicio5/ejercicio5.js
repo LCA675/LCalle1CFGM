3e371b0526fc69ab2d8bea791df7662dd5f2c037
LCA675 <lauracalleanguita@gmail.com>
2025-01-21 14:05:35 +0100

    Ejercicio 5 bloque4

---
+++ bloque4/ejercicio5/ejercicio5.js
@@ -0,0 +1,10 @@
+function numero() {
+    let numeros=parseInt(Math.random()*3);
+    if (numeros==0) {
+        window.location="https://www.microsoft.com/es-es/microsoft-365/outlook/email-and-calendar-software-microsoft-outlook"
+    } else if (numeros==1) {
+        window.location="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+    } else if (numeros==2) {
+        window.location="https://login.yahoo.com/?.src=ym&lang=es-ES&done=https%3A%2F%2Fmail.yahoo.com%2F%3F.lang%3Des-ES%26guce_referrer%3DaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8%26guce_referrer_sig%3DAQAAAM3WJRO4JHfrJlGjmHBt7IO31Iabs9BVVW0IpvutKBT-IVwSWtl1wHqZs3g-O2Zmv5p6o8ITah_4lyKn0qgiKQY1ulBGE8ToEiFax8DbMYZjIxhxy0ICcG61dq99TZgEJy9UInxXIDAw_kXZHgTUSb6rOmgL8zorb_xJTitwtGWP"
+    }
+}
